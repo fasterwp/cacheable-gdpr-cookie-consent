1dde168be361ac6cd6a651c9c4c4d95b029beb32
Andrei Chira <andreichira777@gmail.com>
2022-10-10 13:02:14 +0300

    Update gdpr.js

--- gdpr.js
+++ gdpr.js
@@ -14,9 +14,9 @@ jQuery(window).load(function() {
                 locale: 'en', //This is the default value
                 timeout: 500, //Time until the cookie bar appears
                 expiration: 365, //This is the default value, in days
-                domain: '.closte.com', //If you run the same cookie notice on all subdomains, define the main domain starting with a .
+                domain: 'www.sursadevest.ro', //If you run the same cookie notice on all subdomains, define the main domain starting with a .
                 implicit: true, //Accept cookies on scroll
-                statement: 'https://closte.com/legal/privacy-policy', //Link to your cookie statement page
+                statement: 'https://www.sursadevest.ro/politica-de-confidentialitate/', //Link to your cookie statement page
                 performance: [], //Cookies in the performance category.
                 analytics: ['_ga','_gid','_gat'], //Cookies in the analytics category.
                 marketing: [] //Cookies in the marketing category.
